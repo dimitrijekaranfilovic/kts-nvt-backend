381f695834bbe1ec117b977cd95c018abe8b0bc6
njmarko <marko.njegomir@uns.ac.rs>
2021-11-08 12:55:08 +0100

    Added Model mapper. Added converters in support package.

---
+++ src/main/java/com/ktsnvt/ktsnvt/support/IConverter.java
@@ -0,0 +1,12 @@
+package com.ktsnvt.ktsnvt.support;
+
+import org.springframework.core.convert.converter.Converter;
+
+import java.util.Collection;
+import java.util.stream.Stream;
+
+public interface IConverter<TFrom, TTo> extends Converter<TFrom, TTo> {
+    Collection<TTo> convert(Collection<TFrom> from);
+
+    Stream<TTo> convert(Stream<TFrom> from);
+}
