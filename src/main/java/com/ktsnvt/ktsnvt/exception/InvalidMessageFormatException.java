c6046e6aae07bed8ef6678f916a6dcdb76458d9a
Ivan <nighteliteace@gmail.com>
2021-11-13 16:04:59 +0100

    Added initial version ov WS communication.

---
+++ src/main/java/com/ktsnvt/ktsnvt/exception/InvalidMessageFormatException.java
@@ -0,0 +1,7 @@
+package com.ktsnvt.ktsnvt.exception;
+
+public class InvalidMessageFormatException extends BusinessException{
+    public InvalidMessageFormatException(String message) {
+        super(message);
+    }
+}
