c71478fc154498e8008b9b424d69ffe8a5dc3dfa
Ivan <nighteliteace@gmail.com>
2021-11-30 13:00:33 +0100

    Changed class name.

--- src/test/java/com/ktsnvt/ktsnvt/integration/service/SectionServiceTest.java
+++ src/test/java/com/ktsnvt/ktsnvt/integration/service/SectionServiceTest.java
@@ -13,7 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SectionService {
+class SectionServiceTest {
 
     @Autowired
     private SectionServiceImpl sectionService;
