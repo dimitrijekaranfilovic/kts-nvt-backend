e7ca049a8193ff1644d67f372aeae458424b4439
njmarko <marko.njegomir@uns.ac.rs>
2021-12-04 12:54:59 +0100

    Added provider for email service unit test.

--- src/test/java/com/ktsnvt/ktsnvt/unit/service/EmailServiceTest.java
+++ src/test/java/com/ktsnvt/ktsnvt/unit/service/EmailServiceTest.java
@@ -3,6 +3,9 @@ package com.ktsnvt.ktsnvt.unit.service;
 import com.ktsnvt.ktsnvt.model.SuperUser;
 import com.ktsnvt.ktsnvt.service.impl.EmailServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +13,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
 import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 import static org.mockito.Mockito.*;
 
@@ -41,16 +45,29 @@ class EmailServiceTest {
         verify(javaMailSender, times(1)).send(newMessage);
     }
 
-    @Test
-    void sendMonthlyFinancialReport_calledWithValidData_isSuccess() {
-        // GIVEN
+    @SuppressWarnings("unused")
+    private static Stream<Arguments> provideValidParamsForSendMonthlyFinancialReport() {
         var paramSuperUser = new SuperUser();
         paramSuperUser.setEmail("SU email");
         paramSuperUser.setName("SU name");
         paramSuperUser.setSurname("SU surname");
-        var paramTotalSalaryExpense = new BigDecimal(42);
-        var paramTotalOrderIncome = new BigDecimal("3.14");
-        var paramTotalOrderCost = new BigDecimal(322);
+        return Stream.of(
+                Arguments.of(paramSuperUser, new BigDecimal(42),
+                        new BigDecimal("3.14"), new BigDecimal(322)),
+                Arguments.of(paramSuperUser, new BigDecimal("3.14"),
+                        new BigDecimal(322), new BigDecimal(0)),
+                Arguments.of(paramSuperUser, new BigDecimal(21),
+                        new BigDecimal(42), new BigDecimal(21))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideValidParamsForSendMonthlyFinancialReport")
+    void sendMonthlyFinancialReport_calledWithValidData_isSuccess(SuperUser paramSuperUser,
+                                                                  BigDecimal paramTotalSalaryExpense,
+                                                                  BigDecimal paramTotalOrderIncome,
+                                                                  BigDecimal paramTotalOrderCost) {
+        // GIVEN
         var subject = "Monthly financial report";
         var emailServiceSpy = spy(emailService);
         doNothing().when(emailServiceSpy).sendSimpleMessage(eq(paramSuperUser.getEmail()), eq(subject), anyString());
