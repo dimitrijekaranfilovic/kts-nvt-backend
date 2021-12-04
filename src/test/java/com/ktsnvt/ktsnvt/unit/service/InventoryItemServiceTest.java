f6183190b22d03d48427320cf5e86685aaed9f56
njmarko <marko.njegomir@uns.ac.rs>
2021-12-04 23:34:59 +0100

    Added unit tests for updating of inventoryItem

--- src/test/java/com/ktsnvt/ktsnvt/unit/service/InventoryItemServiceTest.java
+++ src/test/java/com/ktsnvt/ktsnvt/unit/service/InventoryItemServiceTest.java
@@ -3,7 +3,9 @@ package com.ktsnvt.ktsnvt.unit.service;
 import com.ktsnvt.ktsnvt.exception.InventoryItemNameAlreadyExistsException;
 import com.ktsnvt.ktsnvt.exception.InventoryItemNotFoundException;
 import com.ktsnvt.ktsnvt.exception.UsedInventoryItemDeletionException;
+import com.ktsnvt.ktsnvt.model.BasePrice;
 import com.ktsnvt.ktsnvt.model.InventoryItem;
+import com.ktsnvt.ktsnvt.model.enums.ItemCategory;
 import com.ktsnvt.ktsnvt.repository.InventoryItemRepository;
 import com.ktsnvt.ktsnvt.service.BasePriceService;
 import com.ktsnvt.ktsnvt.service.LocalDateTimeService;
@@ -15,10 +17,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
@@ -156,6 +159,91 @@ class InventoryItemServiceTest {
         verify(orderItemService, times(1)).hasActiveOrderItems(inventoryItem);
         verifyNoInteractions(basePriceService);
         verifyNoInteractions(menuItemService);
+    }
+
+
+    @Test
+    void update_whenCalledWithUniqueInventoryItemName_isSuccess() {
+        // GIVEN
+        var id = Integer.valueOf(42);
+        var name = "valid name";
+        var description = "valid description";
+        var allergies = "valid allergies";
+        var image = "valid image";
+        var itemCategory = ItemCategory.DRINK;
+        var basePrice = BigDecimal.valueOf(322);
+
+        var inventoryItem = new InventoryItem("different name", "different description",
+                "different image", "different allergies", ItemCategory.FOOD, Boolean.TRUE);
+        inventoryItem.setId(id);
+
+        var existingBasePrice = new BasePrice(LocalDateTime.of(2021, 12, 1, 3, 14),
+                null, BigDecimal.valueOf(42), inventoryItem);
+        inventoryItem.addBasePrice(existingBasePrice);
+
+        var currentDate = LocalDateTime.of(2021, 12, 3, 2, 2);
+
+        var inventoryItemServiceSpy = spy(inventoryItemService);
+
+        doReturn(inventoryItem).when(inventoryItemServiceSpy).readForUpdate(id);
+        doReturn(Optional.empty()).when(inventoryItemRepository).findByName(name);
+        doNothing().when(basePriceService).updateInventoryItemBasePrice(eq(id), any(BasePrice.class));
+        doReturn(currentDate).when(localDateTimeService).currentTime();
+
+        // WHEN
+        inventoryItemServiceSpy.update(id, name, description, allergies, image, itemCategory, basePrice);
+
+        // THEN
+        assertEquals(id, inventoryItem.getId());
+        assertEquals(name, inventoryItem.getName());
+        assertEquals(description, inventoryItem.getDescription());
+        assertEquals(allergies, inventoryItem.getAllergies());
+        assertEquals(image, inventoryItem.getImage());
+        assertEquals(itemCategory, inventoryItem.getCategory());
+        verify(inventoryItemServiceSpy, times(1)).readForUpdate(id);
+        verify(inventoryItemRepository, times(1)).findByName(name);
+        verify(basePriceService, times(1))
+                .updateInventoryItemBasePrice(eq(id), any(BasePrice.class));
+        verify(localDateTimeService, times(1)).currentTime();
+    }
+
+    @Test
+    void update_whenCalledWithTakenInventoryItemName_throwsException() {
+        // GIVEN
+        var id = Integer.valueOf(42);
+        var name = "valid name";
+        var description = "valid description";
+        var allergies = "valid allergies";
+        var image = "valid image";
+        var itemCategory = ItemCategory.DRINK;
+        var basePrice = BigDecimal.valueOf(322);
+        var inventoryItem = new InventoryItem("different name", "different description",
+                "different image", "different allergies", ItemCategory.FOOD, Boolean.TRUE);
+        inventoryItem.setId(id);
+
+        var inventoryItemSameName = new InventoryItem();
+        inventoryItemSameName.setId(322);
+        inventoryItemSameName.setName(name);
+
+        var inventoryItemServiceSpy = spy(inventoryItemService);
+        doReturn(inventoryItem).when(inventoryItemServiceSpy).readForUpdate(id);
+        doReturn(Optional.of(inventoryItemSameName)).when(inventoryItemRepository).findByName(name);
+
+        // WHEN
+        assertThrows(InventoryItemNameAlreadyExistsException.class,
+                () -> inventoryItemServiceSpy.update(id, name, description, allergies, image, itemCategory, basePrice));
+
+        // THEN
+        assertEquals(id, inventoryItem.getId());
+        assertNotEquals(name, inventoryItem.getName());
+        assertNotEquals(description, inventoryItem.getDescription());
+        assertNotEquals(allergies, inventoryItem.getAllergies());
+        assertNotEquals(image, inventoryItem.getImage());
+        assertNotEquals(itemCategory, inventoryItem.getCategory());
+        verify(inventoryItemServiceSpy, times(1)).readForUpdate(id);
+        verify(inventoryItemRepository, times(1)).findByName(name);
+        verifyNoInteractions(basePriceService);
+        verifyNoInteractions(localDateTimeService);
     }
 
 }
