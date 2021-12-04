c325e9522ff9a3ad0ce7d3987f661eae6c281a62
njmarko <marko.njegomir@uns.ac.rs>
2021-12-05 00:25:39 +0100

    Added unit tests for creation of menuItem

--- src/test/java/com/ktsnvt/ktsnvt/unit/service/MenuItemServiceTest.java
+++ src/test/java/com/ktsnvt/ktsnvt/unit/service/MenuItemServiceTest.java
@@ -1,6 +1,7 @@
 package com.ktsnvt.ktsnvt.unit.service;
 
 import com.ktsnvt.ktsnvt.exception.MenuItemNotFoundException;
+import com.ktsnvt.ktsnvt.model.InventoryItem;
 import com.ktsnvt.ktsnvt.model.MenuItem;
 import com.ktsnvt.ktsnvt.repository.MenuItemRepository;
 import com.ktsnvt.ktsnvt.service.InventoryItemService;
@@ -12,6 +13,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -66,6 +69,38 @@ class MenuItemServiceTest {
 
         // THEN
         verify(menuItemRepository, times(1)).findOneForUpdate(id);
+    }
+
+    @Test
+    void createMenuItem_calledWithValidArguments_isSuccess() {
+        // GIVEN
+        var price = BigDecimal.valueOf(42);
+        var inventoryItemId = Integer.valueOf(42);
+        var inventoryItem = new InventoryItem();
+        inventoryItem.setId(inventoryItemId);
+        var currentTime = LocalDateTime.of(2021, 12, 1, 3, 14);
+        var menuItem = new MenuItem(price, currentTime, null, inventoryItem);
+        menuItem.setId(322);
+
+        var menuItemServiceSpy = spy(menuItemService);
+        doReturn(inventoryItem).when(inventoryItemService).readForUpdate(inventoryItemId);
+        doNothing().when(menuItemServiceSpy).removeActiveMenuItemForInventoryItem(inventoryItemId);
+        doReturn(currentTime).when(localDateTimeService).currentTime();
+        doReturn(menuItem).when(menuItemRepository).save(any(MenuItem.class));
+
+        // WHEN
+        var returnedMenuItem = menuItemServiceSpy.createMenuItem(price, inventoryItemId);
+
+        // THEN
+        assertEquals(menuItem, returnedMenuItem);
+        assertEquals(Boolean.TRUE, inventoryItem.getIsInMenu());
+        assertEquals(returnedMenuItem, menuItem);
+        verify(inventoryItemService, times(1)).readForUpdate(inventoryItemId);
+        verify(menuItemServiceSpy, times(1))
+                .removeActiveMenuItemForInventoryItem(inventoryItemId);
+        verify(localDateTimeService, times(1)).currentTime();
+        verify(menuItemRepository, times(1)).save(any(MenuItem.class));
+
     }
 
 }
