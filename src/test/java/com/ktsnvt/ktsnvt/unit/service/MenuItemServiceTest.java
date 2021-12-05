92d1bc2942c590fb8730cb0a532337dfc26ea764
njmarko <marko.njegomir@uns.ac.rs>
2021-12-05 01:17:40 +0100

    Added unit tests for deactivating menu item

--- src/test/java/com/ktsnvt/ktsnvt/unit/service/MenuItemServiceTest.java
+++ src/test/java/com/ktsnvt/ktsnvt/unit/service/MenuItemServiceTest.java
@@ -1,6 +1,8 @@
 package com.ktsnvt.ktsnvt.unit.service;
 
+import com.ktsnvt.ktsnvt.exception.EntityAlreadyDeactivatedException;
 import com.ktsnvt.ktsnvt.exception.MenuItemNotFoundException;
+import com.ktsnvt.ktsnvt.exception.UsedMenuItemDeletionException;
 import com.ktsnvt.ktsnvt.model.InventoryItem;
 import com.ktsnvt.ktsnvt.model.MenuItem;
 import com.ktsnvt.ktsnvt.repository.MenuItemRepository;
@@ -17,8 +19,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
@@ -149,6 +150,87 @@ class MenuItemServiceTest {
         assertEquals(menuItem, returnedMenuItem);
         verify(menuItemServiceSpy, times(1)).readForUpdate(menuItemId);
         verify(menuItemServiceSpy, times(1)).createMenuItem(price, inventoryItem.getId());
+    }
+
+    @Test
+    void deactivateMenuItem_calledWithActiveInventoryItemWithNoActiveOrders_isSuccess() {
+        // GIVEN
+        var id = Integer.valueOf(42);
+        var menuItem = new MenuItem();
+        menuItem.setId(id);
+        var inventoryItem = new InventoryItem();
+        inventoryItem.addMenuItem(menuItem);
+
+        var currentTime = LocalDateTime.of(2021, 12, 1, 3, 14);
+
+        var menuItemServiceSpy = spy(menuItemService);
+        doReturn(menuItem).when(menuItemServiceSpy).readForUpdate(id);
+        doReturn(Boolean.FALSE).when(orderItemService).hasActiveOrderItems(menuItem);
+        doReturn(currentTime).when(localDateTimeService).currentTime();
+
+        // WHEN
+        menuItemServiceSpy.deactivateMenuItem(id);
+
+        // THEN
+        assertEquals(currentTime, menuItem.getEndDate());
+        assertEquals(Boolean.FALSE, menuItem.getIsActive());
+        assertEquals(Boolean.FALSE, inventoryItem.getIsInMenu());
+        verify(menuItemServiceSpy, times(1)).readForUpdate(id);
+        verify(orderItemService, times(1)).hasActiveOrderItems(menuItem);
+    }
+
+    @Test
+    void deactivateMenuItem_calledWithAlreadyDeactivatedMenuItem_throwsException() {
+        // GIVEN
+        var id = Integer.valueOf(42);
+        var menuItem = new MenuItem();
+        menuItem.setId(id);
+        var inventoryItem = new InventoryItem();
+        inventoryItem.addMenuItem(menuItem);
+        var currentTime = LocalDateTime.of(2021, 12, 1, 3, 14);
+        menuItem.deactivateMenuItem(currentTime);
+        var newCurrentTime = LocalDateTime.of(2021, 12, 3, 2, 2);
+
+        var menuItemServiceSpy = spy(menuItemService);
+        doReturn(menuItem).when(menuItemServiceSpy).readForUpdate(id);
+        doReturn(Boolean.FALSE).when(orderItemService).hasActiveOrderItems(menuItem);
+        doReturn(newCurrentTime).when(localDateTimeService).currentTime();
+
+        // WHEN
+        assertThrows(EntityAlreadyDeactivatedException.class, () -> menuItemServiceSpy.deactivateMenuItem(id));
+
+        // THEN
+        assertNotEquals(newCurrentTime, menuItem.getEndDate());
+        verify(menuItemServiceSpy, times(1)).readForUpdate(id);
+        verifyNoInteractions(orderItemService);
+        verifyNoInteractions(localDateTimeService);
+    }
+
+    @Test
+    void deactivateMenuItem_calledWithMenuItemWithActiveOrderItems_throwsException() {
+        // GIVEN
+        var id = Integer.valueOf(42);
+        var menuItem = new MenuItem();
+        menuItem.setId(id);
+        var inventoryItem = new InventoryItem();
+        inventoryItem.addMenuItem(menuItem);
+        var newCurrentTime = LocalDateTime.of(2021, 12, 3, 2, 2);
+
+        var menuItemServiceSpy = spy(menuItemService);
+        doReturn(menuItem).when(menuItemServiceSpy).readForUpdate(id);
+        doReturn(Boolean.TRUE).when(orderItemService).hasActiveOrderItems(menuItem);
+        doReturn(newCurrentTime).when(localDateTimeService).currentTime();
+
+        // WHEN
+        assertThrows(UsedMenuItemDeletionException.class, () -> menuItemServiceSpy.deactivateMenuItem(id));
+
+        // THEN
+        assertNull(menuItem.getEndDate());
+        assertEquals(Boolean.TRUE, menuItem.getIsActive());
+        assertEquals(Boolean.TRUE, inventoryItem.getIsInMenu());
+        verify(menuItemServiceSpy, times(1)).readForUpdate(id);
+        verify(orderItemService, times(1)).hasActiveOrderItems(menuItem);
+        verifyNoInteractions(localDateTimeService);
     }
 
 }
