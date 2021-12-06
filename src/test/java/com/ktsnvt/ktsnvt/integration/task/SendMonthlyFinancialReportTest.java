bb9c2f1ed62089d70f843e56ea46586017561adf
njmarko <marko.njegomir@uns.ac.rs>
2021-12-06 07:21:41 +0100

    Small refactor

--- src/test/java/com/ktsnvt/ktsnvt/integration/task/SendMonthlyFinancialReportTest.java
+++ src/test/java/com/ktsnvt/ktsnvt/integration/task/SendMonthlyFinancialReportTest.java
@@ -4,10 +4,12 @@ import com.ktsnvt.ktsnvt.task.SendMonthlyFinancialReport;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
+@Transactional
 class SendMonthlyFinancialReportTest {
 
     @Autowired
