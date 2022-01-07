efb0b959874dcc6b8f9f702123dc5aad6fda8a66
Dusan Erdeljan <dusanerdeljan99@gmail.com>
2022-01-07 18:35:09 +0100

    Added e2e tests for employees page.

---
+++ src/test/java/com/ktsnvt/ktsnvt/e2e/tests/EmployeesPageTest.java
@@ -0,0 +1,69 @@
+package com.ktsnvt.ktsnvt.e2e.tests;
+
+import com.ktsnvt.ktsnvt.e2e.tests.pageobjects.EmployeesPage;
+import com.ktsnvt.ktsnvt.e2e.tests.pageobjects.LoginPage;
+import com.ktsnvt.ktsnvt.e2e.tests.pageobjects.Navbar;
+import com.ktsnvt.ktsnvt.e2e.tests.utilities.Utilities;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.support.PageFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EmployeesPageTest extends BaseE2ETest {
+
+    @Test
+    void employeePage_happyFlow() throws InterruptedException {
+        var driver = initDriver();
+        var navbar = PageFactory.initElements(driver, Navbar.class);
+        var loginPage = PageFactory.initElements(driver, LoginPage.class);
+        var employeesPage = PageFactory.initElements(driver, EmployeesPage.class);
+
+        navbar.navigateLogin();
+        loginPage.setEmail("email2@email.com");
+        loginPage.setPassword("password");
+        loginPage.clickLogin();
+
+        assertTrue(Utilities.checkUrl(driver, "/employees"));
+
+        assertTrue(employeesPage.checkEmployeeTableRows(3));
+
+        employeesPage.search("sv", 10.0, 1000.0);
+        assertTrue(employeesPage.checkEmployeeTableRows(1));
+
+        employeesPage.search("sv", 800.0, 1000.0);
+        assertTrue(employeesPage.checkEmployeeTableRows(0));
+
+        employeesPage.resetSearchForm();
+        assertTrue(employeesPage.checkEmployeeTableRows(3));
+
+        employeesPage.clickCreateEmployee();
+        employeesPage.setName("pera");
+        employeesPage.setSurname("peric");
+        employeesPage.setPin("9999");
+        employeesPage.setSalary(300.0);
+        employeesPage.clickSaveButton();
+        assertTrue(employeesPage.checkEmployeeTableRows(4));
+        assertEquals("pera", employeesPage.getLastEmployeeName());
+        assertEquals("300", employeesPage.getLastEmployeeSalary());
+
+        employeesPage.clickUpdateLastEmployee();
+        employeesPage.setName("NOVO IME");
+        employeesPage.clickSaveButton();
+        assertTrue(employeesPage.checkEmployeeTableRows(4));
+        Thread.sleep(500);
+        assertEquals("NOVO IME", employeesPage.getLastEmployeeName());
+
+        employeesPage.clickUpdateLastEmployeeSalary();
+        employeesPage.setUpdateSalary(420.0);
+        employeesPage.clickSaveSalaryButton();
+        assertTrue(employeesPage.checkEmployeeTableRows(4));
+        Thread.sleep(500);
+        assertEquals("420", employeesPage.getLastEmployeeSalary());
+
+        employeesPage.clickDeleteLastEmployee();
+        employeesPage.clickYesButton();
+        assertTrue(employeesPage.checkEmployeeTableRows(3));
+
+    }
+
+}
---
+++ src/test/java/com/ktsnvt/ktsnvt/e2e/tests/pageobjects/EmployeesPage.java
@@ -0,0 +1,150 @@
+package com.ktsnvt.ktsnvt.e2e.tests.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class EmployeesPage extends BasePage {
+
+    @FindBy(css = "input[formcontrolname='query']")
+    private WebElement queryInput;
+
+    @FindBy(css = "input[formcontrolname='salaryLowerBound']")
+    private WebElement salaryLowerBoundInput;
+
+    @FindBy(css = "input[formcontrolname='salaryUpperBound']")
+    private WebElement salaryUpperBoundInput;
+
+    @FindBy(css = "mat-select[formcontrolname='type']")
+    private WebElement typeSelect;
+
+    @FindBy(css = "button[id='search']")
+    private WebElement searchButton;
+
+    @FindBy(css = "button[id='reset']")
+    private WebElement resetButton;
+
+    @FindBy(css = "button[id='createEmployee']")
+    private WebElement createEmployeeButton;
+
+    @FindBy(css = "input[formcontrolname='name']")
+    private WebElement nameInput;
+
+    @FindBy(css = "input[formcontrolname='surname']")
+    private WebElement surnameInput;
+
+    @FindBy(css = "input[formcontrolname='pin']")
+    private WebElement pinInput;
+
+    @FindBy(css = "input[formcontrolname='salary']")
+    private WebElement salaryInput;
+
+    @FindBy(css = "button[id='saveEmployee']")
+    private WebElement saveButton;
+
+    @FindBy(css = "button[id='saveSalary']")
+    private WebElement saveSalaryButton;
+
+    @FindBy(css = "input[id='updateSalary']")
+    private WebElement updateSalaryInput;
+
+    @FindBy(css = "button[id='yes']")
+    private WebElement yesButton;
+
+    public EmployeesPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void search(String query, Double salaryLowerBound, Double salaryUpperBound) throws InterruptedException {
+        sendKeys(queryInput, query);
+        sendKeys(salaryLowerBoundInput, salaryLowerBound.toString());
+        sendKeys(salaryUpperBoundInput, salaryUpperBound.toString());
+        click(searchButton);
+        Thread.sleep(200);
+    }
+
+    public boolean checkEmployeeTableRows(int numberOfElements) {
+        var elements = (new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.numberOfElementsToBe(By.cssSelector("tbody tr"), numberOfElements)));
+        return elements.size() == numberOfElements;
+    }
+
+    public void resetSearchForm() throws InterruptedException {
+        click(resetButton);
+        Thread.sleep(200);
+    }
+
+    public void clickCreateEmployee() {
+        click(createEmployeeButton);
+    }
+
+    public void setName(String name) {
+        sendKeys(nameInput, name);
+    }
+
+    public void setSurname(String surname) {
+        sendKeys(surnameInput, surname);
+    }
+
+    public void setPin(String pin) {
+        sendKeys(pinInput, pin);
+    }
+
+    public void setSalary(Double salary) {
+        sendKeys(salaryInput, salary.toString());
+    }
+
+    public void clickSaveButton() {
+        click(saveButton);
+    }
+
+    public void clickSaveSalaryButton() {
+        click(saveSalaryButton);
+    }
+
+    public void setUpdateSalary(Double salary) {
+        sendKeys(updateSalaryInput, salary.toString());
+    }
+
+    public void clickYesButton() {
+        click(yesButton);
+    }
+
+    public String getLastEmployeeName() {
+        var lastTableRow = getLastTableRow();
+        var tds = lastTableRow.findElements(By.cssSelector("td"));
+        return tds.get(0).getText();
+    }
+
+    public String getLastEmployeeSalary() {
+        var lastTableRow = getLastTableRow();
+        var tds = lastTableRow.findElements(By.cssSelector("td"));
+        return tds.get(4).getText();
+    }
+
+    public void clickUpdateLastEmployee() {
+        performLastEmployeeAction(0);
+    }
+
+    public void clickUpdateLastEmployeeSalary() {
+        performLastEmployeeAction(1);
+    }
+
+    public void clickDeleteLastEmployee() {
+        performLastEmployeeAction(2);
+    }
+
+    private void performLastEmployeeAction(int index) {
+        var lastTableRow = getLastTableRow();
+        var actions = lastTableRow.findElements(By.cssSelector("button"));
+        click(actions.get(index));
+    }
+
+    private WebElement getLastTableRow() {
+        return  (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//table[1]/tbody/tr)[last()]")));
+    }
+}
