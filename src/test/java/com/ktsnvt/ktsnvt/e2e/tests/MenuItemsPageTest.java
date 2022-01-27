a14dcdb3c17d29e4cb43f5bfb2702f4a747b65f2
njmarko <marko.njegomir@uns.ac.rs>
2022-01-27 17:51:40 +0100

    Added navigation to the menuItems page.

--- src/test/java/com/ktsnvt/ktsnvt/e2e/tests/MenuItemsPageTest.java
+++ src/test/java/com/ktsnvt/ktsnvt/e2e/tests/MenuItemsPageTest.java
@@ -1,7 +1,37 @@
 package com.ktsnvt.ktsnvt.e2e.tests;
 
-public class MenuItemsPageTest {
+import com.ktsnvt.ktsnvt.e2e.tests.pageobjects.LoginPage;
+import com.ktsnvt.ktsnvt.e2e.tests.pageobjects.Navbar;
+import com.ktsnvt.ktsnvt.e2e.tests.utilities.Utilities;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.openqa.selenium.support.PageFactory;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class MenuItemsPageTest extends BaseE2ETest {
+
+    @Test
+    void menuItemsHappyFlow() throws InterruptedException {
+        var driver = initDriver();
+        var navbar = PageFactory.initElements(driver, Navbar.class);
+        var loginPage = PageFactory.initElements(driver, LoginPage.class);
+        var menuItemsPage = PageFactory.initElements(driver, MenuItemsPageTest.class);
 
 
+        navbar.navigateLogin();
+        loginPage.setEmail("email2@email.com");
+        loginPage.setPassword("password");
+        loginPage.clickLogin();
+
+        assertTrue(Utilities.checkUrl(driver, "/employees"));
+
+        navbar.navigateMenuItems();
+        assertTrue(Utilities.checkUrl(driver, "/menu-items"));
+
+
+        driver.close();
+    }
 
 }
